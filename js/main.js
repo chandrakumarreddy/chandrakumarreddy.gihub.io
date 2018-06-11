d181b7b8b2ceab0bc184be720917a7b4d4678399
Chandrakumarreddy Penugonda <chandrapenugonda655@gmail.com>
2018-06-11 19:00:15 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function(){
+	$('#slider-carousel').carouFredSel({
+         width:'100%',
+         responsive:true,
+         circular:true,
+         scroll:{
+                 items:1,
+                 duration:500,
+                 pauseOnHover:true  
+         },
+         auto:true,
+         items:
+         {
+             visible:{
+                   min:1,
+                   max:1
+             },
+             height:"variable"
+         },
+         pagination:{
+             container:".sliderpager",
+             pageAnchorBuilder:false
+         }
+     });
+    $(window).scroll(function(){
+        let top=$(window).scrollTop();
+        if(top>=60){
+            $("header").addClass("nav-active");
+        }else{
+            if($("header").hasClass("nav-active")){
+                $("header").removeClass("nav-active");
+            }
+        }
+    })
+});
